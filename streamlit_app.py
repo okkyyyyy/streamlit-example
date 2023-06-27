b57cfd6e03e6aa12f247a92c214013df53a7fda5
okkyyyyy <137767856+okkyyyyy@users.noreply.github.com>
2023-06-27 10:42:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,21 +6,5 @@ st.set_page_config(
     page_icon="✋",
 )
 
-st.title("to do list")
-menu = ["Create","Read","Update","Delete","About"]
-choice = st.sidebar.selectbox("Menu",menu)
 
-if choice == "Create":
-    st.subheader("Add Items")
-
-#Layout
-col1,col2 = st.beta_columns(2)
-
-with col1:
-    task = st.text_area("Task To Do")
-
-with col2: 
-    task_status = st.selectbox("Status")
-    task_due_date = st.date_input("Due Date")
-                                
 
